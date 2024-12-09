08f136c57b8bf2ff8e077e6a30dfbc3b44b86dbb
Sri-Galaxy <vulapusrinath@gmail.com>
2024-12-09 15:06:04 +0530

    third commit

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3 as tts
 
-recog = sr.Recognizer()
 engine = tts.init()
 
-def speak(text):
+def response(text):
     engine.say(text)
     engine.runAndWait()
 
+
+def request(c):
+    if "open google" in c:
+        wb.open("https://google.com")
+    elif "open youtube" in c:
+        wb.open("https://youtube.com")
+
+
 if __name__ == "__main__":
-    speak("Hello Sri I am Asher!")
+    response("Hello Sri I am Asher!")
 
     while True:
         r = sr.Recognizer()
@@ -24,13 +31,16 @@ if __name__ == "__main__":
             word = r.recognize_google(audio)
 
             if(word.lower() == "asher"):
-                speak("Jo hukum mere aaka..!")
+                response("Yes Boss")
 
                 with sr.Microphone() as source:
                     print("Asher Active..!")
                     audio = r.listen(source, timeout=2, phrase_time_limit=1)
                     command = r.recognize_google(audio)
+
+                    request(command.lower())
+
         except Exception as e:
-            print(e)
+            print(f"ERROR: {e}")
 
 
