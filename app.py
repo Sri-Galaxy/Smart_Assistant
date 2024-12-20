5e1236c720694d03cb9678c0cfe3216f8c504dd2
Srinath <vulapusrinath@gmail.com>
2024-12-20 12:12:41 +0530

    Jarvis added

--- app.py
+++ app.py
@@ -9,19 +9,39 @@ def speak(text):
     engine.runAndWait()
 
 
+def processCommand(c):
+    if "open google" in c.lower():
+        wb.open("https://google.com")
+    elif "open youtube" in c.lower():
+        wb.open("https://youtube.com")
+    else:
+        pass
+        
+
 if __name__ == "__main__":
-    speak("This is Asher")
+    speak("----------This is Jarvis!----------")
+    print("----------This is Jarvis!----------")
 
     while True:
         r = sr.Recognizer()
         
-        print("Recognising...")
         try:
             with sr.Microphone() as source:
-                print("Say Something..!")
-                audio = r.listen(source, timeout=5, phrase_time_limit=5)
-            command = r.recognize_google(audio)
-            print(command)
+                print("\nSay Something..!\n")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            
+            if(word.lower() == "jarvis"):
+                print("Yes Boss..\n")
+                speak("Yes Boss!")
+                
+                with sr.Microphone() as source:
+                    print("Jarvis is Active..!")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+                    
         except Exception as e:
             print(f"Error: {e}")
 
