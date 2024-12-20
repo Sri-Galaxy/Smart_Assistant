b18725734f3726f523b52e9b7698c374488f6597
Srinath <vulapusrinath@gmail.com>
2024-12-20 11:17:26 +0530

    added

--- app.py
+++ app.py
@@ -2,49 +2,27 @@ import speech_recognition as sr
 import webbrowser as wb
 import pyttsx3 as tts
 
-engine = tts.init()
+engine  = tts.init()
 
-def res(text):
+def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 
-def req(c):
-    if "open google" in c:
-        wb.open("https://google.com")
-    elif "open youtube" in c:
-        wb.open("https://youtube.com")
-    elif "open netflix" in c:
-        wb.open("https://netflix.com")
-    else:
-        pass
-
-
 if __name__ == "__main__":
-    res("Hello Sri I am Asher!")
+    speak("This is Asher")
 
     while True:
         r = sr.Recognizer()
-
-        print("Recognising the word...")
+        
+        print("Recognising...")
         try:
             with sr.Microphone() as source:
-                print("Say something..!")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
-
-            word = r.recognize_google(audio)
-
-            if(word.lower() == "asher"):
-                res("Yes Boss")
-
-                with sr.Microphone() as source:
-                    print("Asher Active..!")
-                    audio = r.listen(source, timeout=2, phrase_time_limit=1)
-                    command = r.recognize_google(audio)
-
-                    req(command.lower())
-
+                print("Say Something..!")
+                audio = r.listen(source, timeout=5, phrase_time_limit=5)
+            command = r.recognize_google(audio)
+            print(command)
         except Exception as e:
-            print(f"ERROR: {e}")
+            print(f"Error: {e}")
 
 
